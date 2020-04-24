63ca3ec7fb933ee64f6fd3cb6a99fe5ae4d2f15b
deeksha207 <54803390+deeksha207@users.noreply.github.com>
2020-04-24 23:24:42 +0530

    Update web.js

--- web.js
+++ web.js
@@ -2,3 +2,16 @@ let det=document.getElementById('det').addEventListener('click',show_me);
 function show_me(){
     window.open('web2.html',"_self");
 }
+let click_1=document.getElementById("click_me").addEventListener("click",show1);
+function show1(){
+    let show=document.getElementById("links");
+    let down=document.getElementById("down1");
+    if(show.style.display==="block"){
+        show.style.display="none";
+        down.style.marginTop="50px";
+    }
+    else{
+        show.style.display="block";
+        down.style.marginTop="240px";
+    }
+}
